082a8f2a9aedb6f17bd422d4b4fa739e7de972f0
vince-fugnitto <vincent.fugnitto@ericsson.com>
2022-10-02 12:22:02 -0400

    feat: add `Serie A Standings` command
    The commit adds support for the `Serie A Standings` command which opens
    an external link to show Juventus standings.
    
    Signed-off-by: vince-fugnitto <vincent.fugnitto@ericsson.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,23 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('vscode-juve.fixturesResults', () => {
-        vscode.env.openExternal(vscode.Uri.parse('https://www.juventus.com/en/teams/first-team-men/fixtures-results/'));
 
-    });
+    const disposables: vscode.Disposable[] = [];
 
-    context.subscriptions.push(disposable);
+    disposables.push(
+        vscode.commands.registerCommand('vscode-juve.fixtures-results', () => {
+            vscode.env.openExternal(vscode.Uri.parse('https://www.google.com/search?q=juventus+standings&oq=juventus+standings&aqs=chrome.0.0i20i263i512j0i512j0i20i263i512j0i512j0i10i512j0i22i30l2j69i60.3738j1j4&sourceid=chrome&ie=UTF-8#sie=t;/m/045xx;2;/m/03zv9;mt;fp;1;;;'));
+        })
+    );
+
+    disposables.push(
+        vscode.commands.registerCommand('vscode-juve.serie-a-standings', () => {
+            vscode.env.openExternal(vscode.Uri.parse('https://www.google.com/search?q=juventus+standings&oq=juventus+standings&aqs=chrome.0.0i20i263i512j0i512j0i20i263i512j0i512j0i10i512j0i22i30l2j69i60.3738j1j4&sourceid=chrome&ie=UTF-8#sie=t;/m/045xx;2;/m/03zv9;st;fp;1;;;'));
+        })
+    );
+
+
+    context.subscriptions.push(...disposables);
 }
 
 export function deactivate() { }
